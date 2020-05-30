ab30567f1fa865c4a37c174c3da6e0986d06c90d
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-05-30 15:42:27 -0300

    Aula 02 - Primeiro teste

--- __tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-
-test('example', () => {
-  const result = sum(3 , 4)
-
-  expect(result).toBe(7);
-})
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // import { Container } from './styles';
 
 export default function TechList() {
-  return <div />;
+  const [techs, setTechs] = useState([]);
+
+  function handleAddSubmit() {
+    setTechs([...techs, 'Node.js']);
+  }
+
+  return (
+    <div>
+      <ul data-testid="tech-list">
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddSubmit}>Adicionar</button>
+    </div>
+  );
 }
 
