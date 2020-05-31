8cc449d8f8777767febbc33ba77b46f62d7f5f2c
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-05-31 11:34:10 -0300

    Aula 06 - Mock do useDispatch

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
 
 import TechList from '../../src/components/TechList';
+
+import { addTech } from '../../src/store/modules/techs/actions';
 
 jest.mock('react-redux');
 
@@ -18,5 +20,20 @@ describe('TechList component', () => {
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
+  });
+
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js'}});
+    fireEvent.submit(getByTestId('tech-form'));
+
+    console.log(dispatch.mock.calls);
+
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'));
   })
 })
