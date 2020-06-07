96e8d0693d216716d0483440611b514f83573bb3
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-06-06 21:37:43 -0300

    Aula 09 - Mock do axios

--- __tests__/store/sagas/techs.test.js
+++ __tests__/store/sagas/techs.test.js
@@ -1,13 +1,29 @@
 import { runSaga } from 'redux-saga';
-import { getTechsSuccess } from '../../../src/store/modules/techs/actions';
+import MockAdapter from 'axios-mock-adapter';
+import api from '../../../src/services/api';
+
+import { getTechsSuccess, getTechsFailure } from '../../../src/store/modules/techs/actions';
 import { getTechs } from '../../../src/store/modules/techs/sagas';
+
+const apiMock =  new MockAdapter(api);
 
 describe('Techs saga', () => {
   it('should be able to fetch techs', async() => {
     const dispatch = jest.fn();
 
+    apiMock.onGet('techs').reply(200, ['Node.js'])
+
     await runSaga({ dispatch }, getTechs).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']));
+  })
+  it('should fail when api return error', async() => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('techs').reply(500)
+
+    await runSaga({ dispatch }, getTechs).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure());
   })
 })
