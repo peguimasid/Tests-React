d957a3696e1df18cc39071fc65335927d521a5eb
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-06-06 22:04:15 -0300

    Aula 10 - Coverage report

--- __tests__/store/reducers/techs.test.js
+++ __tests__/store/reducers/techs.test.js
@@ -2,6 +2,12 @@ import reducer, { INITIAL_STATE } from '../../../src/store/modules/techs/reducer
 import { addTech } from '../../../src/store/modules/techs/actions';
 
 describe('Techs reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  })
+
   it('ADD_TECH', () => {
     const state = reducer(INITIAL_STATE, addTech('Node.js'));
 
